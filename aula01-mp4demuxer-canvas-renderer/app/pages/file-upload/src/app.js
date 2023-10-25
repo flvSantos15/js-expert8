ad8f68285f1b405da84252588fbb17d0bcdc77dd
flvSantos15 <flvSantos300@gmail.com>
2023-10-25 17:50:01 -0300

    feat: demuxer aula1 in progress

---
+++ aula01-mp4demuxer-canvas-renderer/app/pages/file-upload/src/app.js
@@ -0,0 +1,39 @@
+import Clock from './deps/clock.js'
+import View from './view.js'
+
+const view = new View()
+const clock = new Clock()
+
+let took = ''
+view.configureOnFileChange((file) => {
+  clock.start((time) => {
+    took = time
+    view.updateElapsedTime(`Process started ${time}`)
+  })
+
+  setTimeout(() => {
+    clock.stop()
+    view.updateElapsedTime(`Process took ${took.replace('ago', '')}`)
+  }, 5000)
+})
+
+async function fakeFetch() {
+  const filePath = '/videos/frag_bunny.mp4'
+  const response = await fetch(filePath)
+
+  // traz tamanho do arquivo
+  // const response = await fetch(filePath, {
+  //     method: 'HEAD'
+  // })
+  // response.headers.get('content-length')
+  const file = new File([await response.blob()], filePath, {
+    type: 'video/mp4',
+    lastModified: Date.now()
+  })
+  const event = new Event('change')
+  Reflect.defineProperty(event, 'target', { value: { files: [file] } })
+  document.getElementById('fileUpload').dispatchEvent(event)
+}
+
+fakeFetch()
+// parei no 33:33 (criação do webworker)
